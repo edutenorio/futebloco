d97c04d9b0dcbf2e71d0e69de5abb37639f15b2e
Eduardo Tenorio Bastos <59711031+edutenorio@users.noreply.github.com>
2024-01-03 21:19:53 +0000

    debug match

--- futebloco/settings.py
+++ futebloco/settings.py
@@ -21,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-if DEBUG:
-    load_dotenv(os.path.join(BASE_DIR, '.env'))
+# load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
